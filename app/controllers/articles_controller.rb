4e38e7371d3528cdb48bcf3ad34956e9c7e62106
ryuji0606 <magic.hour006@gmail.com>
2020-03-07 01:43:38 +0900

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,16 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-  # GET /articles
-  # GET /articles.json
+  
   def index
     @articles = Article.all
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
+  
   def show
   end
 
-  # GET /articles/new
+  
   def new
     @article = Article.new
   end
